c0b73578021c7c8f2029673108473020bc68559d
Eduard Musatov <pkboyar09@gmail.com>
2024-05-20 09:43:54 +0300

    copy data into separate folder and file, implement IntensiveOverviewPage, fixes

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 import { FC } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
-import { Intensive } from '../utils/types/Intensive'
 
+
+import { intensives } from '../data/intensives'
 
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage'
 import SignInPage from '../pages/SignInPage/SignInPage'
@@ -16,49 +17,6 @@ import IntensiveTeamsPage from '../pages/IntensiveTeamsPage/IntensiveTeamsPage'
 import IntensiveContextProvider from '../context/IntensivesContext'
 
 const App: FC = () => {
-
-  const intensives: Intensive[] = [
-    {
-      id: 1,
-      name: "Веб-разработка",
-      descr: "Интенсив по современным технологиям веб-разработки.",
-      openDate: new Date('2024-06-01'),
-      closeDate: new Date('2024-07-01'),
-      flow: "Поток 20-ИCбо"
-    },
-    {
-      id: 2,
-      name: "Погружение в бэк-разработку",
-      descr: "Интенсив по созданию серверных приложений и API.",
-      openDate: new Date('2024-07-05'),
-      closeDate: new Date('2024-08-05'),
-      flow: "Поток 20-ИCбо"
-    },
-    {
-      id: 3,
-      name: "Разработка игр",
-      descr: "Интенсив по разработке компьютерных игр с использованием Unity и Unreal Engine.",
-      openDate: new Date('2024-08-10'),
-      closeDate: new Date('2024-09-10'),
-      flow: "Поток 20-ИCбо"
-    },
-    {
-      id: 4,
-      name: "Мобильная разработка",
-      descr: "Интенсив по созданию мобильных приложений для iOS и Android.",
-      openDate: new Date('2024-09-15'),
-      closeDate: new Date('2024-10-15'),
-      flow: "Поток 20-ИCбо"
-    },
-    {
-      id: 5,
-      name: "Дизайн и UX",
-      descr: "Интенсив по основам дизайна и пользовательского опыта.",
-      openDate: new Date('2024-10-20'),
-      closeDate: new Date('2024-11-20'),
-      flow: "Поток 20-ИCбо"
-    }
-  ]
 
   return (
     <div className='App'>
--- src/context/IntensivesContext.tsx
+++ src/context/IntensivesContext.tsx
@@ -8,6 +8,8 @@ interface IntensiveProviderProps {
    children: ReactNode
 }
 const IntensiveContextProvider: FC<IntensiveProviderProps> = ({ intensives, children }) => {
+   // можно прописать дополнительную бизнес логику (например, методы удаления, добавления) и передать методы в value, также надо будет создать состояние intensives, чтобы оно внутри провайдера изменялось
+   // ну и соответственно надо будет дополнительно тип у контекста изменить, потому что там будут не только intensives
    return (
       <IntensivesContext.Provider value={intensives}> {children} </IntensivesContext.Provider>
    )
