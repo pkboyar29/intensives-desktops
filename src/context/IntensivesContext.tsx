7fdd8e0f6d59435eccf0e49a915d3a513fee04e0
Eduard Musatov <pkboyar09@gmail.com>
2024-05-24 16:14:57 +0300

    fixes + implement storing current user and sign in authorization and logout button and displaying FIO in header

--- src/context/IntensivesContext.tsx
+++ src/context/IntensivesContext.tsx
@@ -1,17 +1,49 @@
-import { FC, ReactNode, createContext } from 'react'
+import { FC, ReactNode, createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 import { Intensive } from '../utils/types/Intensive'
+import Cookies from 'js-cookie'
 
-export const IntensivesContext = createContext<Intensive[]>([])
+interface IntensiveContextType {
+   intensives: Intensive[],
+   getIntensives: () => void
+}
+
+export const IntensivesContext = createContext<IntensiveContextType>({
+   intensives: [],
+   getIntensives: () => { }
+})
 
 interface IntensivesContextProviderProps {
-   intensives: Intensive[],
    children: ReactNode
 }
-const IntensivesProvider: FC<IntensivesContextProviderProps> = ({ intensives, children }) => {
+const IntensivesProvider: FC<IntensivesContextProviderProps> = ({ children }) => {
+
+   const [intensives, setIntensives] = useState<Intensive[]>([])
+
+   const getIntensives = (): void => {
+      // {
+      //    headers: {
+      //       'Authorization': `Bearer ${Cookies.get('access')}`
+      //    }
+      // }
+      axios.get(`${process.env.REACT_APP_BACKEND_URL}/intensives/`)
+         .then(response => {
+            const filteredIntensives: Intensive[] = response.data.results.map((item: any) => ({
+               id: item.id,
+               name: item.name,
+               description: item.description,
+               is_open: item.is_open,
+               open_dt: item.open_dt.split('T')[0],
+               close_dt: item.close_dt.split('T')[0]
+            }))
+            setIntensives(filteredIntensives)
+         })
+   }
+
    // можно прописать дополнительную бизнес логику (например, методы удаления, добавления) и передать методы в value, также надо будет создать состояние intensives, чтобы оно внутри провайдера изменялось
    // ну и соответственно надо будет дополнительно тип у контекста изменить, потому что там будут не только intensives
    return (
-      <IntensivesContext.Provider value={intensives}> {children} </IntensivesContext.Provider>
+      <IntensivesContext.Provider value={{ intensives, getIntensives }}> {children} </IntensivesContext.Provider>
    )
 }
 
--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,19 +1,48 @@
-import './SignInPage.css'
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+import './SignInPage.css'
+import { CurrentUserContext } from '../../context/CurrentUserContext'
+
+interface SignInProps {
+   email: string,
+   password: string
+}
 
 const SignInPage: FC = () => {
 
+   const { updateCurrentUser } = useContext(CurrentUserContext)
+
    const navigate = useNavigate()
+
+   const { handleSubmit, register } = useForm<SignInProps>({
+      mode: "onBlur"
+   })
+
+   const onSubmit = (data: SignInProps) => {
+      console.log(data)
+      axios.post(`${process.env.REACT_APP_BACKEND_URL}/token/`, data)
+         .then(response => {
+            console.log(response.data)
+            Cookies.set('refresh', response.data.refresh)
+            Cookies.set('access', response.data.access)
+            updateCurrentUser(response.data.access)
+            navigate('/intensives')
+         })
+         .catch(error => console.log(error.response.data))
+   }
 
    return (
       <>
          <div className='sign-in'>
-            <form className='sign-in__form'>
+            <form onSubmit={handleSubmit(onSubmit)} className='sign-in__form'>
                <div className="form__title">Войти в систему</div>
-               <input className='sign-in__input' type='text' placeholder='Логин' />
-               <input className='sign-in__input' type='password' placeholder='Пароль' />
-               <button onClick={() => navigate('/intensives')} className='sign-in__button' type='submit'>ВОЙТИ</button>
+               <input {...register('email')} className='sign-in__input' type='text' placeholder='Email' />
+               <input {...register('password')} className='sign-in__input' type='password' placeholder='Пароль' />
+               <button className='sign-in__button' type='submit'>ВОЙТИ</button>
             </form>
          </div>
       </>
