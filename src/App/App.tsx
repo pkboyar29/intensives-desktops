8b1650fba8f4b7ceecf131fe19061dc2c0429140
Eduard Musatov <pkboyar09@gmail.com>
2024-09-07 09:22:43 +0300

    separate my Routes into routeConfig file

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,38 +1,9 @@
 import { FC, useEffect, useContext } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { CurrentUserContext } from '../context/CurrentUserContext';
 
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-import SignInPage from '../pages/SignInPage/SignInPage';
 import Header from '../components/Header/Header';
-import IntensivesPage from '../pages/IntensivesPage/IntensivesPage';
-import TeacherMainPage from '../pages/TeacherMainPage/TeacherMainPage';
-import EventsPage from '../pages/EventsPage/EventsPage';
-import IntensiveOverviewPage from '../pages/IntensiveOverviewPage/IntensiveOverviewPage';
-import TeamsPage from '../pages/TeamsPage/TeamsPage';
-import EventOverviewPage from '../pages/EventOverviewPage/EventOverviewPage';
-import TeamEvaluationPage from '../pages/TeamEvaluationPage/TeamEvaluationPage';
-import StudentMainPage from '../pages/StudentMainPage/StudentMainPage';
-import TeamOverviewPage from '../pages/TeamOverviewPage/TeamOverviewPage';
-import StudentTasksBoardPage from '../pages/StudentTasksBoardPage/StudentTasksBoardPage';
-import StudentTasksPage from '../pages/StudentTasksPage/StudentTasksPage';
-import StudentTaskAnswerPage from '../pages/StudentTaskAnswerPage/StudentTaskAnswerPage';
-import EvaluationIntensivePage from '../pages/EvaluationIntensivePage/EvaluationIntensivePage';
-
-import StatisticsIntensive from '../pages/StatisticsIntensive';
-import CreateEvent from '../pages/CreateEvent';
-import ManageProfile from '../pages/ManageLists/ManageProfile';
-import ManageRoles from '../pages/ManageLists/ManageRole';
-import Commands from '../components/Commands';
-import { Intensiv } from '../pages/Intensive';
-import Plan from '../pages/Plan';
-import Intensives from '../pages/Intensives';
-import ManageMenu from '../pages/ManageMenu';
-import CreateCommand from '../pages/CreateCommand';
-import ListCreateIntensiv from '../components/ListCreateIntensiv';
-
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
+import routeConfig from '../router/routeConfig';
 
 const App: FC = () => {
   const { updateCurrentUser } = useContext(CurrentUserContext);
@@ -46,53 +17,13 @@ const App: FC = () => {
       <Header />
 
       <Routes>
-        <Route path="/teacher/:intensiveId" element={<TeacherMainPage />}>
-          <Route path="overview" element={<IntensiveOverviewPage />} />
-          <Route path="teams" element={<TeamsPage />} />
-          <Route path="events" element={<EventsPage />} />
-          <Route path="events/:eventId" element={<EventOverviewPage />} />
-          <Route
-            path="team-evaluation/:eventId/:teamId"
-            element={<TeamEvaluationPage />}
-          />
-          {/* <Route path='education-requests' element={<EducationRequestsPage />} />
-          <Route path='education-requests/:requestId' element={<EducationRequestOverviewPage />} /> */}
-        </Route>
-
-        <Route path="/student/:teamId" element={<StudentMainPage />}>
-          <Route path="overview" element={<TeamOverviewPage />} />
-          <Route path="intensiv-overview" element={<IntensiveOverviewPage />} />
-          <Route path="events" element={<EventsPage />} />
-          <Route path="events/:eventId" element={<EventOverviewPage />} />
-          <Route path="tasks-board" element={<StudentTasksBoardPage />} />
-          <Route path="tasks" element={<StudentTasksPage />} />
-          <Route path="task/:taskId" element={<StudentTaskAnswerPage />} />
-        </Route>
-
-        <Route path="/evaluation" element={<EvaluationIntensivePage />} />
-        <Route path="/intensives1" element={<IntensivesPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/" element={<Navigate to="/sign-in" />} />
-        <Route path="*" element={<NotFoundPage />} />
-
-        <Route path="/intensives" element={<Intensives />} />
-        <Route path="/manageMenu" element={<ManageMenu />} />
-        <Route path="/plan" element={<Plan />} />
-        <Route path="/createIntensive" element={<ListCreateIntensiv />} />
-        <Route path="/intensiv" element={<Intensiv />} />
-        <Route path="/commands" element={<Commands />} />
-        <Route
-          path="/createCom"
-          element={
-            <DndProvider backend={HTML5Backend}>
-              <CreateCommand />
-            </DndProvider>
-          }
-        />
-        <Route path="/editRoles" element={<ManageRoles />} />
-        <Route path="/editProfile" element={<ManageProfile />} />
-        <Route path="/editEvent" element={<CreateEvent />} />
-        <Route path="/statisticsIntensive" element={<StatisticsIntensive />} />
+        {routeConfig.map((route) => (
+          <Route key={route.id} path={route.path} element={route.element}>
+            {route.children?.map((childRoute) => (
+              <Route path={childRoute.path} element={childRoute.element} />
+            ))}
+          </Route>
+        ))}
       </Routes>
     </div>
   );
---
+++ src/router/routeConfig.tsx
@@ -0,0 +1,188 @@
+import { RouteObject, Navigate } from 'react-router-dom';
+
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import SignInPage from '../pages/SignInPage/SignInPage';
+import Header from '../components/Header/Header';
+import IntensivesPage from '../pages/IntensivesPage/IntensivesPage';
+import TeacherMainPage from '../pages/TeacherMainPage/TeacherMainPage';
+import EventsPage from '../pages/EventsPage/EventsPage';
+import IntensiveOverviewPage from '../pages/IntensiveOverviewPage/IntensiveOverviewPage';
+import TeamsPage from '../pages/TeamsPage/TeamsPage';
+import EventOverviewPage from '../pages/EventOverviewPage/EventOverviewPage';
+import TeamEvaluationPage from '../pages/TeamEvaluationPage/TeamEvaluationPage';
+import StudentMainPage from '../pages/StudentMainPage/StudentMainPage';
+import TeamOverviewPage from '../pages/TeamOverviewPage/TeamOverviewPage';
+import StudentTasksBoardPage from '../pages/StudentTasksBoardPage/StudentTasksBoardPage';
+import StudentTasksPage from '../pages/StudentTasksPage/StudentTasksPage';
+import StudentTaskAnswerPage from '../pages/StudentTaskAnswerPage/StudentTaskAnswerPage';
+import EvaluationIntensivePage from '../pages/EvaluationIntensivePage/EvaluationIntensivePage';
+
+import StatisticsIntensive from '../pages/StatisticsIntensive';
+import CreateEvent from '../pages/CreateEvent';
+import ManageProfile from '../pages/ManageLists/ManageProfile';
+import ManageRoles from '../pages/ManageLists/ManageRole';
+import Commands from '../components/Commands';
+import { Intensiv } from '../pages/Intensive';
+import Plan from '../pages/Plan';
+import Intensives from '../pages/Intensives';
+import ManageMenu from '../pages/ManageMenu';
+import CreateCommand from '../pages/CreateCommand';
+import ListCreateIntensiv from '../components/ListCreateIntensiv';
+
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
+
+type RouteType = RouteObject & {
+  requiredAuth: boolean;
+};
+
+const routeConfig: RouteType[] = [
+  {
+    path: '/teacher/:intensiveId',
+    element: <TeacherMainPage />,
+    children: [
+      {
+        path: 'overview',
+        element: <IntensiveOverviewPage />,
+      },
+      {
+        path: 'teams',
+        element: <TeamsPage />,
+      },
+      {
+        path: 'events',
+        element: <EventsPage />,
+      },
+      {
+        path: 'events/:eventId',
+        element: <EventOverviewPage />,
+      },
+      {
+        path: 'team-evaluation/:eventId/:teamId',
+        element: <TeamEvaluationPage />,
+      },
+    ],
+    requiredAuth: false,
+  },
+  {
+    path: '/student/:teamId',
+    element: <StudentMainPage />,
+    children: [
+      {
+        path: 'overview',
+        element: <TeamOverviewPage />,
+      },
+      {
+        path: 'intensiv-overview',
+        element: <IntensiveOverviewPage />,
+      },
+      {
+        path: 'events',
+        element: <EventsPage />,
+      },
+      {
+        path: 'events/:eventId',
+        element: <EventOverviewPage />,
+      },
+      {
+        path: 'tasks-board',
+        element: <StudentTasksBoardPage />,
+      },
+      {
+        path: 'tasks',
+        element: <StudentTasksPage />,
+      },
+      {
+        path: 'task/:taskId',
+        element: <StudentTaskAnswerPage />,
+      },
+    ],
+    requiredAuth: false,
+  },
+  {
+    path: '/evaluation',
+    element: <EvaluationIntensivePage />,
+    requiredAuth: false,
+  },
+  {
+    path: '/intensives1',
+    element: <IntensivesPage />,
+    requiredAuth: false,
+  },
+  {
+    path: '/sign-in',
+    element: <SignInPage />,
+    requiredAuth: false,
+  },
+  {
+    path: '/',
+    element: <Navigate to="/sign-in" />,
+    requiredAuth: false,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+    requiredAuth: false,
+  },
+  {
+    path: '/intensives',
+    element: <Intensives />,
+    requiredAuth: false,
+  },
+  {
+    path: '/manageMenu',
+    element: <ManageMenu />,
+    requiredAuth: false,
+  },
+  {
+    path: '/plan',
+    element: <Plan />,
+    requiredAuth: false,
+  },
+  {
+    path: '/createIntensive',
+    element: <ListCreateIntensiv />,
+    requiredAuth: false,
+  },
+  {
+    path: '/intensiv',
+    element: <Intensiv />,
+    requiredAuth: false,
+  },
+  {
+    path: '/commands',
+    element: <Commands />,
+    requiredAuth: false,
+  },
+  {
+    path: '/createCom',
+    element: (
+      <DndProvider backend={HTML5Backend}>
+        <CreateCommand />
+      </DndProvider>
+    ),
+    requiredAuth: false,
+  },
+  {
+    path: '/editRoles',
+    element: <ManageRoles />,
+    requiredAuth: false,
+  },
+  {
+    path: '/editProfile',
+    element: <ManageProfile />,
+    requiredAuth: false,
+  },
+  {
+    path: '/editEvent',
+    element: <CreateEvent />,
+    requiredAuth: false,
+  },
+  {
+    path: '/statisticsIntensive',
+    element: <StatisticsIntensive />,
+    requiredAuth: false,
+  },
+];
+
+export default routeConfig;
