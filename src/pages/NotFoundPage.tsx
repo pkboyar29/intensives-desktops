94e18e3b91dd7a83b622409501b0a825532205d6
Eduard Musatov <pkboyar09@gmail.com>
2025-04-04 22:05:18 +0300

    MultipleSelectInput fixes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 
 const NotFoundPage: FC = () => {
-  return <div className="text-red">404 NOT FOUND</div>;
+  return <div className="text-red pt-[88px]">404 NOT FOUND</div>;
 };
 
 export default NotFoundPage;
